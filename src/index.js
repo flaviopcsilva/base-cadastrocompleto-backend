7a65f399470487a159a951bdbbf29a34725bd3f7
Flávio Peres <flaviopc2@gmail.com>
2024-08-09 13:01:51 -0300

    Criado o Cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ require('dotenv').config()
 const express = require('express')
 const rotas = require('./rotas')
 const app = express()
+const cors = require('cors')
+
+// Configuração para permitir CORS apenas do seu domínio frontend
+
+
+const corsOptions = {
+    origin: ['https://painel-transporthos.vercel.app', 'https://painel-transporthos.vercel.app/enviar-pdf', 'http://localhost:4200'], // Substitua pelo seu domínio frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Erro interno do servidor');
+});
+
+// Middleware para lidar com o preflight request
+app.options('*', cors(corsOptions));
 
 app.use(express.json())
 
